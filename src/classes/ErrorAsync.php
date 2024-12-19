fc139324e109da96a3d9288cb140256347926560
Felix on Lenovo <info@felixdesmeules.com>
2024-12-18 22:25:20 -0500

    Fixed a notice being triggered on some env

--- src/classes/ErrorAsync.php
+++ src/classes/ErrorAsync.php
@@ -7,7 +7,7 @@ class ErrorAsync {
     static function shutdown() {
 
         $error = error_get_last();
-        if (!is_null($error) && ($error['type'] === E_ERROR || $error['type'] === E_PARSE)) {
+        if (!empty($error['type']) && ($error['type'] === E_ERROR || $error['type'] === E_PARSE)) {
             error_log($error['message']);
         }
     }
